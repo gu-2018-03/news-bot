f3d9fcf258e2814061cd1182db908c53f887c924
machine23 <vassili.baranov@gmail.com>
2018-04-12 10:11:36 +0300

    реализован AsyncMyTeleBotDB

--- src/MyTeleBot/mytelebot_db.py
+++ src/MyTeleBot/mytelebot_db.py
@@ -1,7 +1,7 @@
 import pymongo
+from motor import motor_asyncio
+import time
 
-
-DEBUG = False
 
 class MyTeleBotDB:
     '''
@@ -84,34 +84,84 @@ class MyTeleBotDB:
             return 0.0
         last_time = last_news['published']
         return last_time
+
+
+class AsyncMyTeleBotDB:
+    '''
+    класс для работы с БД.
+    '''
+    def __init__(self):
+        self.client = motor_asyncio.AsyncIOMotorClient()
+        self.db = self.client.telebot
+        self.required = {'title', 'published', 'link', 'summary', 'base'}
         
+    async def get_news(self, key='', count=10) -> []:
+        '''
+        Принимает на вход:
+        key   - (пока не используется)
+        count - количество возвращаемых новостей
+
+        Возвращает список из словарей, каждый словарь - отдельная новость.
+        Список отсортирован по времени публикации - от более новой новости
+        к более старой.
+        Словарь имеет следующие поля:
+        {
+            'title':     -заголовок новости,
+            'published': -таймстамп времени публикации - тип float,
+            'link':      -адрес новости,
+            'summary':   -краткое изложение новости,
+            'base':      -адрес rss-канала
+        }
+        '''
+        cursor = self.db.news.find() \
+                   .sort('published', pymongo.DESCENDING) 
+        news = await cursor.to_list(count)
+        return news
+
+    async def set_news(self, news: dict):
+        '''
+        Принимает обязательный параметр news типа dict.
+        Обязательные поля в словаре:
+        {
+            'title':     -заголовок новости,
+            'published': -таймстамп времени публикации - тип float,
+            'link':      -адрес новости,
+            'summary':   -краткое изложение новости,
+            'base':      -адрес rss-канала
+        }
+        Также можно добавлять другие поля с дополнительной информацией.
+        '''
+        if not isinstance(news, dict):
+            error = 'expected dict instance, {} found'.format(type(news))
+            raise TypeError(error)
+
+        if not self.required <= set(news):
+            error = 'not found required key(s) {}' \
+                .format(self.required - set(news))
+            raise TypeError(error)
+
+        if not isinstance(news['published'], float):
+            error = "a float is required for 'published' (got type {})" \
+                .format(type(news['published']))
+            raise TypeError(error)
+
+        await self.db.news.insert_one(news)
+
+    async def get_last_published(self, base):
+        '''
+        Возвращает время публикации последней новости для данного rss-канала.
+        Если в БД для канала еще нет новостей, возвращает 0.0
+        base - адрес rss-канала.
+        '''
+        cursor = self.db.news.find({'base': base}, {'published': 1}) \
+                             .sort('published', pymongo.DESCENDING) \
+                             .limit(1)
+        last_news = await cursor.to_list(1)
+        if not last_news:
+            return 0.0
+        last_time = last_news[0]['published']
+        return last_time
+
 
 if __name__ == '__main__':
-    # Примитивное тестирование
-    # Надо определиться с библиотекой для тестов
-    if DEBUG:
-        db = MyTeleBotDB()
-        db.db.news.drop()
-        db.set_news(
-            {
-                'title': 'First news',
-                'published': 1234.55,
-                'link': 'https://bla.bla/novost_dnja',
-                'summary': 'lorem ipsum',
-                'base': 'https://bla.bla/rss',
-            }
-        )
-        try:
-            db.set_news(
-                {
-                    'title': 'First news',
-                    'published': 1234.55,
-                    'summary': 'lorem ipsum',
-                    'base': 'https://bla.bla/rss',
-                }
-            )
-        except TypeError as err:
-            print(err)
-        print(list(db.get_news()))
-        print(db.get_last_published('http://xyz.net')) # вернет 0.0
-        print(db.get_last_published('https://bla.bla/rss')) 
+    pass
