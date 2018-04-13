7406725b7bfeacf81725a38b04b5c358adeb30d0
Кирилл Владимиров <kvladimirov.main@gmail.com>
2018-04-14 00:43:38 +0300

    customize news template

--- src/MyTeleBot/mytelebot.py
+++ src/MyTeleBot/mytelebot.py
@@ -3,6 +3,7 @@ import telebot
 import constants
 from mytelebot_db import MyTeleBotDB
 from mytoken import TOKEN
+from emoji import emojize
 
 
 class MyTeleBot:
@@ -22,14 +23,19 @@ class MyTeleBot:
 
         @self.bot.message_handler(content_types=['text'])
         def handle_message(message):
-            self.bot.send_message(message.chat.id, self.format_news(), parse_mode='HTML')
+            self.bot.send_message(
+                message.chat.id,
+                self.format_news(),
+                parse_mode='HTML',
+                disable_web_page_preview=1
+            )
 
     def format_news(self):
         news = self.db.get_news()
         if len(news) == 0:
             return (constants.BASE_EMPTY)
         return '\n\n'.join(
-            [constants.TEMPLATE.format(**n) for n in news])
+            [constants.TEMPLATE.format(**n, emoji=emojize(":arrow_right:", use_aliases=True)) for n in news])
 
     def run(self):
         self.bot.polling(none_stop=True, interval=0)
