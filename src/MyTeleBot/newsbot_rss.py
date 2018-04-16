d4f75804df9c4c57cc44e52063f926de724572f1
machine23 <vassili.baranov@gmail.com>
2018-04-16 11:46:28 +0300

    убранна очередь новостей

--- src/MyTeleBot/newsbot_rss.py
+++ src/MyTeleBot/newsbot_rss.py
@@ -21,21 +21,21 @@ RSS_CHANNELS = {
 
 
 class Rss_source(object):
-    '''
+    """
     класс храняций список источников, в дальнейшем сюда добавится последнняя
     прочитанная новость, чтобы избежать дублирования
-    '''
+    """
     def __init__(self, channels):
         self.channels = channels
         self.left_to_process = len(channels)
 
 
-async def read_feed(db, feeds_queue, news_queue, rss_source):
-    '''
+async def read_feed(db, feeds_queue, rss_source):
+    """
     фунция, читающая полученный rss-файл (развибает его на записи) и передающая
     обработчику (processor), в качестве параметров принимает очередь rss-feeds,
     очередь новостей и объект хранящий список каналов
-    '''
+    """
     left_to_process = rss_source.left_to_process
     while (left_to_process > 0):
         if not feeds_queue.empty():
@@ -43,18 +43,18 @@ async def read_feed(db, feeds_queue, news_queue, rss_source):
             if feed:
                 try:
                     rss = feedparser.parse(feed)
-                    await process(db, rss.feed.link, rss.entries, news_queue)
+                    await process(db, rss.feed.link, rss.entries)
                     left_to_process -= 1
                 except Exception as e:
                     print(e)
         await asyncio.sleep(0)
 
 
-async def process(db, feed, entries, news_queue):
-    '''
+async def process(db, feed, entries):
+    """
     функция, обрабатывающая записии, формирующая новости и отправляющая новости
     в очередь новостей, принимает список записей и очередь новостей
-    '''
+    """
     last_published = await db.get_last_published(feed)
     news_count = 0
     for entry in entries:
@@ -66,7 +66,6 @@ async def process(db, feed, entries, news_queue):
             news['published'] = published
             news['summary'] = html2text.html2text(entry['summary'])
             news['base'] = feed
-            # news_queue.put(news)
             try:
                 await db.set_news(news)
                 news_count += 1
@@ -77,26 +76,10 @@ async def process(db, feed, entries, news_queue):
     print('Добавленно {} новостей из {}'.format(news_count, feed))
 
 
-# async def send_to_db(db, news_queue, rss_source):
-#     '''
-#     функция отправляющая новости в базу данных, примнимает базу данных, очередь
-#     новостей и список каналов
-#     '''
-#     while (rss_source.left_to_process > 0) or (not news_queue.empty()):
-#         if news_queue.empty():
-#             await asyncio.sleep(0)
-#         else:
-#             news = news_queue.get()
-#             try:
-#                 await db.set_news(news)
-#             except Exception as e:
-#                 print(e)
-
-
 async def get_data2(channel, feeds_queue):
-    '''
+    """
     функция получающая rss-данные по Http и отравляющая их в очердеь feeds_queue
-    '''
+    """
     async with aiohttp.ClientSession() as session:
         async with async_timeout.timeout(5):
             try:
@@ -110,8 +93,7 @@ async def get_data2(channel, feeds_queue):
 
 
 def main_cycle():
-    ''' основнй цикл '''
-    news_queue = queue.Queue()
+    """ основнй цикл """
     feeds_queue = queue.Queue()
     rss_source = Rss_source(RSS_CHANNELS)
 
@@ -122,8 +104,7 @@ def main_cycle():
 
     tasks = [asyncio.Task(get_data2(channel, feeds_queue))
                 for channel in rss_source.channels.values()]
-    tasks.append(read_feed(db, feeds_queue, news_queue, rss_source)) # db
-    # tasks.append(send_to_db(db, news_queue, rss_source))
+    tasks.append(read_feed(db, feeds_queue, rss_source)) # db
     
     loop.run_until_complete(asyncio.wait(tasks))
 
