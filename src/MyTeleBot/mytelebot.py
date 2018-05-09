0f658939207020332e5bac83e9514c0633240d04
Rassoliny <dimo120@yandex.ru>
2018-05-09 12:47:39 +0000

    telebot_webhook.py -> mytelebot.py

--- src/MyTeleBot/mytelebot.py
+++ src/MyTeleBot/mytelebot.py
@@ -1,10 +1,11 @@
 import telebot
-
+import cherrypy
+from mytoken import TOKEN
 import constants
 from mytelebot_db import MyTeleBotDB
-from mytoken import TOKEN
 from emoji import emojize
 from datetime import datetime
+
 
 class MyTeleBot:
     def __init__(self):
@@ -42,17 +43,62 @@ class MyTeleBot:
 
         return answer
 
+    #webhook handler
+    def process_new_updates(self, *args):
+        self.bot.process_new_updates(*args)
+
+
+
     def run(self):
+
         # remove webhook before started
         self.bot.remove_webhook()
         if constants.WEBHOOK=='YES':
             # setup webhook 
-            self.bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-                      certificate=open(WEBHOOK_SSL_CERT, 'r'))
+            self.bot.set_webhook(url=constants.WEBHOOK_URL_BASE + constants.WEBHOOK_URL_PATH,
+                      certificate=open(constants.WEBHOOK_SSL_CERT, 'r'))
+            # CherryPy server config
+            cherrypy.config.update({
+                'server.socket_host': constants.WEBHOOK_LISTEN,
+                'server.socket_port': constants.WEBHOOK_PORT,
+                'server.ssl_module': 'builtin',
+                'server.ssl_certificate': constants.WEBHOOK_SSL_CERT,
+                'server.ssl_private_key': constants.WEBHOOK_SSL_PRIV
+            })
+
+
+            cherrypy.quickstart(WebhookServer(), constants.WEBHOOK_URL_PATH, {'/': {}})
+
         else:
             self.bot.polling(none_stop=True, interval=0)
+        
+
+
+# webhook-server
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                        'content-type' in cherrypy.request.headers and \
+                        cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            # message handler
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
+
+
+
+
 
 
 if __name__ == '__main__':
     bot = MyTeleBot()
     bot.run()
+
+
+
+
