5f83ce1815df65fea4e7146df417f4070e63fc45
Vassili Baranov <vassili.baranov@gmail.com>
2018-05-23 09:53:12 +0300

    autoformat

--- src/MyTeleBot/mytelebot.py
+++ src/MyTeleBot/mytelebot.py
@@ -28,8 +28,7 @@ class MyTeleBot:
                 message.chat.id,
                 self.format_news(message.text),
                 parse_mode='HTML',
-                disable_web_page_preview=1
-            )
+                disable_web_page_preview=1)
 
     def format_news(self, key=''):
         news = self.db.get_news(key=key)
@@ -37,50 +36,59 @@ class MyTeleBot:
             return (constants.BASE_EMPTY)
         answer = '\n\n'
         for i in news:
-            date = datetime.utcfromtimestamp(i['published']).strftime("%Y-%m-%d %H:%M:%S")
+            date = datetime.utcfromtimestamp(
+                i['published']).strftime("%Y-%m-%d %H:%M:%S")
             payload = constants.TEMPLATE.format(**i)
-            answer = '{} {} {} {} \n\n'.format(answer, emojize(":arrow_right:", use_aliases=True), str(date), payload)
+            answer = '{} {} {} {} \n\n'.format(answer,
+                                               emojize(
+                                                   ":arrow_right:",
+                                                   use_aliases=True),
+                                               str(date), payload)
 
         return answer
 
-    #webhook handler
+    # webhook handler
     def process_new_updates(self, *args):
         self.bot.process_new_updates(*args)
-
-
 
     def run(self):
 
         # remove webhook before started
         self.bot.remove_webhook()
-        if constants.WEBHOOK=='YES':
-            # setup webhook 
-            self.bot.set_webhook(url=constants.WEBHOOK_URL_BASE + constants.WEBHOOK_URL_PATH,
-                      certificate=open(constants.WEBHOOK_SSL_CERT, 'r'))
+        if constants.WEBHOOK == 'YES':
+            # setup webhook
+            self.bot.set_webhook(
+                url=constants.WEBHOOK_URL_BASE + constants.WEBHOOK_URL_PATH,
+                certificate=open(constants.WEBHOOK_SSL_CERT, 'r'))
             # CherryPy server config
             cherrypy.config.update({
-                'server.socket_host': constants.WEBHOOK_LISTEN,
-                'server.socket_port': constants.WEBHOOK_PORT,
-                'server.ssl_module': 'builtin',
-                'server.ssl_certificate': constants.WEBHOOK_SSL_CERT,
-                'server.ssl_private_key': constants.WEBHOOK_SSL_PRIV
+                'server.socket_host':
+                constants.WEBHOOK_LISTEN,
+                'server.socket_port':
+                constants.WEBHOOK_PORT,
+                'server.ssl_module':
+                'builtin',
+                'server.ssl_certificate':
+                constants.WEBHOOK_SSL_CERT,
+                'server.ssl_private_key':
+                constants.WEBHOOK_SSL_PRIV
             })
 
-
-            cherrypy.quickstart(WebhookServer(), constants.WEBHOOK_URL_PATH, {'/': {}})
+            cherrypy.quickstart(WebhookServer(), constants.WEBHOOK_URL_PATH,
+                                {'/': {}})
 
         else:
             self.bot.polling(none_stop=True, interval=0)
-        
 
 
 # webhook-server
 class WebhookServer(object):
     @cherrypy.expose
     def index(self):
-        if 'content-length' in cherrypy.request.headers and \
-                        'content-type' in cherrypy.request.headers and \
-                        cherrypy.request.headers['content-type'] == 'application/json':
+        if ('content-length' in cherrypy.request.headers
+                and 'content-type' in cherrypy.request.headers and
+                cherrypy.request.headers['content-type'] == 'application/json'
+            ):
             length = int(cherrypy.request.headers['content-length'])
             json_string = cherrypy.request.body.read(length).decode("utf-8")
             update = telebot.types.Update.de_json(json_string)
@@ -91,14 +99,6 @@ class WebhookServer(object):
             raise cherrypy.HTTPError(403)
 
 
-
-
-
-
 if __name__ == '__main__':
     bot = MyTeleBot()
     bot.run()
-
-
-
-
